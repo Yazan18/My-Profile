6d896ac84be028556f1ce26aa24f591d765d0009
Yazan18 <76278387+Yazan18@users.noreply.github.com>
2021-03-12 19:19:50 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.body.style.width = screen.width;
+
+// document.getElementById("hero_div").style.height = screen.height
+
+var go_up = document.getElementById("go_up");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    go_up.style.display = "block";
+  } else {
+    go_up.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
